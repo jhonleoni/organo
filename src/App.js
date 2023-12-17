dee72e1d50e0c82197c8504845afe02bbe11bdf5
Jhony Almeida <106479072+jhonleoni@users.noreply.github.com>
2023-12-17 15:00:55 -0400

    feat: team compoonent creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+import React, { useState } from 'react';
+import Time from './componentes/Time';
 
 function App() {
+
+  const [membros, setMembros] = useState([])
+
+  const AoNovoMembroAdicionado = (membro) => {
+    console.log(membro)
+    setMembros([...membros, membro])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoMembroCadastrado={
+        membro => AoNovoMembroAdicionado(membro)}
+      />
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="Data Science"/>
     </div>
   );
 } 
